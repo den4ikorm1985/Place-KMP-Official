477a7efc352c9bfec19dfc60255d0875ba0cf6fe
den4ikorm1985 <Den4ikorm@gmail.com>
2026-02-04 15:01:58 +0500

    ANDROID: Добавлены MainActivity и App.kt (Входные точки)

--- composeApp/src/androidMain/kotlin/org/example/placebooker/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/example/placebooker/MainActivity.kt
@@ -3,12 +3,13 @@ package org.example.placebooker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import org.example.placebooker.App
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App() // Запускаем наш общий UI
+            App() // Это вызывает наш общий код из commonMain
         }
     }
 }
